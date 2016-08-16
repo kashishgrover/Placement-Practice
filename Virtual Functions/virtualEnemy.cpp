8d64c2923f89025652d8d8ad61cadbef7803bc96
Kashish Grover <kashishgrover95@gmail.com>
2016-08-16 22:39:58 +0530

    Changes

--- Virtual Functions/virtualEnemy.cpp
+++ Virtual Functions/virtualEnemy.cpp
@@ -2,23 +2,35 @@
 
 using namespace std;
 
-class Enemy {
+class Enemy {	// Abstract Class
 	public:
 		virtual void attack() {
+		}
+		virtual void set(int){
 		}
 };
 
 class Ninja: public Enemy {
 	public:
+		int a;
 		void attack() {
 			cout<<"Ninja Attack!"<<endl;
+		}
+		void set(int a)
+		{
+			this->a = a;
 		}
 };
 
 class Monster: public Enemy {
 	public:
+		int b;
 		void attack() {
 			cout<<"Monster Attack!"<<endl; 
+		}
+		void set(int b)
+		{
+			this->b=b;
 		}
 };
 
@@ -29,4 +41,8 @@ int main() {
 	Enemy *enemy2 = &m;
 	enemy1->attack();
 	enemy2->attack();
+	n.set(1);
+	cout<<n.a;
+	m.set(2);
+	cout<<m.b;
 }
