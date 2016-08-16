f90aabb886a3ad4334469ee13351fd56d59f5b85
Kashish Grover <Kashish Grover>
2016-08-16 16:46:48 +0530

    Added program for virtual functions

---
+++ Virtual Functions/virtualEnemy.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+class Enemy {
+	public:
+		virtual void attack() {
+		}
+};
+
+class Ninja: public Enemy {
+	public:
+		void attack() {
+			cout<<"Ninja Attack!"<<endl;
+		}
+};
+
+class Monster: public Enemy {
+	public:
+		void attack() {
+			cout<<"Monster Attack!"<<endl;
+		}
+};
+
+int main() {
+	Ninja n;
+	Monster m;
+	Enemy *enemy1 = &n;
+	Enemy *enemy2 = &m;
+	enemy1->attack();
+	enemy2->attack();
+}
